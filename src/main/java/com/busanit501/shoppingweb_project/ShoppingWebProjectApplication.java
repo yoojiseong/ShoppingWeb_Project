b139c6d34968c4dc324313ae6027a5524a092278
yoojiseong <yujiseong588@gmail.com>
2025-07-23 13:53:20 +0900

    first commit

---
+++ src/main/java/com/busanit501/shoppingweb_project/ShoppingWebProjectApplication.java
@@ -0,0 +1,13 @@
+package com.busanit501.shoppingweb_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppingWebProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShoppingWebProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/busanit501/shoppingweb_project/ShoppingWebProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.busanit501.shoppingweb_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingWebProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
