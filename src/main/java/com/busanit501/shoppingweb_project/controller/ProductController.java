16e96691e2c4cffb71c4e5b33f5feaeffdae9016
minjuhyun1999 <minjoohyun67@naver.com>
2025-07-24 09:26:37 +0900

    ProductController 상품 전체 및 단일 조회 API 추가

--- src/main/java/com/busanit501/shoppingweb_project/controller/ProductController.java
+++ src/main/java/com/busanit501/shoppingweb_project/controller/ProductController.java
@@ -7,12 +7,14 @@ import com.busanit501.shoppingweb_project.service.ProductService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-
+import java.util.List;
 
 @RestController
 @RequestMapping("/products")
@@ -26,4 +28,18 @@ public class ProductController {
         ProductResponseDto responseDto = productService.createProduct(requestDto);
         return ResponseEntity.status(HttpStatus.CREATED).body(responseDto);
     }
+
+
+    @GetMapping
+    public ResponseEntity<List<ProductResponseDto>> getAllProducts() {
+        List<ProductResponseDto> products = productService.getAllProducts();
+        return ResponseEntity.ok(products);
+    }
+
+    @GetMapping("/{productId}")
+    public ResponseEntity<ProductResponseDto> getProduct(@PathVariable Long productId) {
+        ProductResponseDto product = productService.getProduct(productId);
+        return ResponseEntity.ok(product);
+    }
+
 }
