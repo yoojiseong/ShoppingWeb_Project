66cb7830a5907a80fadc1473e6e9a1e270e38553
minjuhyun1999 <minjoohyun67@naver.com>
2025-07-24 09:30:07 +0900

    ReviewController 상품별 리뷰 조회 및 단일 리뷰 조회 API 추가

--- src/main/java/com/busanit501/shoppingweb_project/controller/ReviewController.java
+++ src/main/java/com/busanit501/shoppingweb_project/controller/ReviewController.java
@@ -7,13 +7,14 @@ import com.busanit501.shoppingweb_project.service.ReviewService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-
+import java.util.List;
 
 @RestController
 @RequestMapping("/products/{productId}/reviews")
@@ -29,4 +30,18 @@ public class ReviewController {
         ReviewResponseDto responseDto = reviewService.createReview(productId, requestDto);
         return ResponseEntity.status(HttpStatus.CREATED).body(responseDto);
     }
+
+
+    @GetMapping
+    public ResponseEntity<List<ReviewResponseDto>> getAllReviewsByProductId(@PathVariable Long productId) {
+        List<ReviewResponseDto> reviews = reviewService.getAllReviewsByProductId(productId);
+        return ResponseEntity.ok(reviews);
+    }
+
+    @GetMapping("/{reviewId}")
+    public ResponseEntity<ReviewResponseDto> getReview(@PathVariable Long reviewId) {
+        ReviewResponseDto review = reviewService.getReview(reviewId);
+        return ResponseEntity.ok(review);
+    }
+
 }
