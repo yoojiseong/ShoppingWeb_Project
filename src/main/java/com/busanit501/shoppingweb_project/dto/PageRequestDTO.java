62793fe2bc2dd6fd3b843646c87bb9a32c9ec99f
logi0116 <roguewrite001@gmail.com>
2025-07-30 12:21:37 +0900

    feat: 공통 페이징 처리를 위한 DTO 추가

---
+++ src/main/java/com/busanit501/shoppingweb_project/dto/PageRequestDTO.java
@@ -0,0 +1,29 @@
+package com.busanit501.shoppingweb_project.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+
+@Builder
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class PageRequestDTO {
+
+    @Builder.Default
+    private int page = 1;
+
+    @Builder.Default
+    private int size = 10;
+
+    private String type; // 검색 타입: t, c, w, tc, tw, twc
+    private String keyword;
+
+    public Pageable getPageable(String... props) {
+        return PageRequest.of(this.page - 1, this.size, Sort.by(props).descending());
+    }
+}
