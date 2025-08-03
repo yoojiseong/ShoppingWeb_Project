d1673785dc95c990645f18a694264d14355e1511
chanwoo97 <jcw6223@naver.com>
2025-08-03 19:43:25 +0900

    @Column(name = "thumbnail", nullable = false)  "thumbnail" -> "is_thumbnail" 다시 수정

--- src/main/java/com/busanit501/shoppingweb_project/domain/ProductImage.java
+++ src/main/java/com/busanit501/shoppingweb_project/domain/ProductImage.java
@@ -32,7 +32,7 @@ public class ProductImage {
     @JsonBackReference
     private Product product;
 
-    @Column(name = "thumbnail", nullable = false)
+    @Column(name = "is_thumbnail", nullable = false)
     @Builder.Default
     private boolean thumbnail = false;
 
